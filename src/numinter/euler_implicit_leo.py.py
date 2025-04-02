a8889b4e9e909022b484bcd8ed9d894ec40ece20
PolarLeo <leonard.schirner@ltu.se>
2025-04-02 15:38:06 +0200

    implicit leo added

--- src/numinter/euler_implicit_leo.py.py
+++ src/numinter/euler_implicit_leo.py.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from equation import X, Y
-# use explicit euler method
+# use implicit euler method?
 #X = lambda t, p0, m = 1, l=1: p0/(m*l**2)
 #Y = lambda t, phi, m = 1, l=1, omega=1: -m*l**2*omega**2*np.sin(phi) 
 
@@ -24,12 +24,10 @@ t = np.zeros(steps)
 
 for i in range(steps - 1):
     t[i] = t0 + i*h
-    x_vector[i + 1] = x_vector[i] + h*X(t[i], y_vector[i])
-    y_vector[i + 1] = y_vector[i] + h*Y(t[i], x_vector[i])
     # implicit according to 
     # https://en.wikipedia.org/wiki/Explicit_and_implicit_methods#Illustration_using_the_forward_and_backward_Euler_methods
-    # x_vector[i + 1] = (-1 + np.sqrt(1 + 4*h*X(t[i], y_vector[i]))) /(2*h)
-    # y_vector[i + 1] = (-1 + np.sqrt(1 + 4*h*Y(t[i], x_vector[i]))) /(2*h)
+    x_vector[i + 1] = (-1 + np.sqrt(1 + 4*h*X(t[i], y_vector[i]))) /(2*h)
+    y_vector[i + 1] = (-1 + np.sqrt(1 + 4*h*Y(t[i], x_vector[i]))) /(2*h)
 
 plt.figure(figsize = (12, 8))
 plt.plot(x_vector,y_vector, 'bo--', label='Approximate')
