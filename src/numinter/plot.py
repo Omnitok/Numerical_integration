ca261e675b53888c18fe64e56d7a6c1da3abc6a2
János <faszalegeny@gmail.com>
2025-04-04 14:08:03 +0200

    commit before rebase

--- src/numinter/plot.py
+++ src/numinter/plot.py
@@ -1,33 +1,23 @@
-# Plot the data (in terminal)
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from random import randint
-import PIL
 import numpy as np
 
 ###### generate test data
 x = []
 y = []
 z = []
-h = 20
+t  = 20
 for i in range(100):
     x.append(randint(1, 100))
     y.append(randint(1, 100))
     z.append(randint(1, 100))
-print(z)
+#print(z)
 ###############
 
-#fig, ax = plt.subplots()
-#scat = ax.scatter([], []) 
-
-#def animate(i):
-#    scat.set_offsets(np.c_[x[:i+1], y[:i+1]])
-#    return(scat,)
-
-
-def plot_data(x, y, h, z=None):
-#    fig, ax = plt.subplots()
+def plot_data(x, y, t, z=None):
+    
     frames = []
+    
     if z is None:
 #        for i in range(h):
  #           plt.scatter(x[i], y[i])
@@ -60,24 +50,39 @@ def plot_data(x, y, h, z=None):
             ax.set_ylim(min(y), max(y))
             ax.set_title(f"Step {i}")
 
-            # Save the figure to a BytesIO object
+            # use BytesIO object
             buf = BytesIO()
             plt.savefig(buf, format='png')
             buf.seek(0)
             frames.append(imageio.imread(buf))
             buf.close()
             plt.close(fig)
-#            plt.show()
-
-        import imageio.v2 as imageio
+        
+        # save the image as a gif
         imageio.mimsave("test.gif", frames, duration=1, loop=0)
 
     else:
-        for i in range(1, len(x) + 1):
-            
-            ax.scatter(x, y, z)
+        from io import BytesIO
+        import imageio.v2 as imageio
+        for i in range(len(x)):
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection="3d")
+            ax.scatter(x[:i], y[:i], z[:i])
+            ax.set_xlim(min(x), max(x))
+            ax.set_ylim(min(y), max(y))
+            ax.set_zlim(min(z), max(z))
+            ax.set_title(f"Step {i}")
 
-        plt.show()
+            # save
+            buf = BytesIO()
+            plt.savefig(buf, format="png")
+            buf.seek(0)
+            frames.append(imageio.imread(buf))
+            buf.close()
+            plt.close(fig)
 
-plot_data(x, y, h, z)
+        imageio.mimsave("test3d.gif", frames, duration=1, loop=0)
+
+############ TEST the plots
+plot_data(x, y, t, z)
 
