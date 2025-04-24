42570c11fd71cb1e885cf2ec6a98bac810d94f1c
János <faszalegeny@gmail.com>
2025-04-24 13:41:59 +0200

    push before rebase

--- src/numinter/plot.py
+++ src/numinter/plot.py
@@ -20,8 +20,42 @@ def plot_data(*input_data):
     if len(input_data) == 3: # 2D matrix, x, y, and t 
         plt.scatter(x,y,t)
 
+<<<<<<< HEAD
     else: # 3D matrix, x, y, z and t
         plt.scatter(x,y,z,t)
+=======
+            # use BytesIO object
+            buf = BytesIO()
+            plt.savefig(buf, format='png')
+            buf.seek(0)
+            frames.append(imageio.imread(buf))
+            buf.close()
+            plt.close(fig)
+        
+        # save the image as a gif
+        imageio.mimsave("test.gif", frames, duration=1, loop=0)
+
+    else:
+        from io import BytesIO
+        import imageio.v2 as imageio
+        for i in range(len(x)):
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection="3d")
+            ax.scatter(x[:i], y[:i], z[:i])
+            ax.set_xlim(min(x), max(x))
+            ax.set_ylim(min(y), max(y))
+            ax.set_zlim(min(z), max(z))
+            ax.set_title(f"Step {i}")
+
+            # save
+            buf = BytesIO()
+            plt.savefig(buf, format="png")
+            buf.seek(0)
+            frames.append(imageio.imread(buf))
+            buf.close()
+            plt.close(fig)
+        imageio.mimsave("test3d.gif", frames, duration=1, loop=0)
+>>>>>>> e63b96d (push before rebase)
 
 ############ TEST the plots
 #plot_data(*test())
